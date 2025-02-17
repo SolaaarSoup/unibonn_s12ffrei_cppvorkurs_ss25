f263859694991af8f91a13f8f7bd08081ec5eb55
SolarSoup <felixfreis@gmail.com>
2025-02-17 15:50:14 +0100

    multitabelle fertig

--- exercises/03_kontrollfluss/source/multitabelle.cpp
+++ exercises/03_kontrollfluss/source/multitabelle.cpp
@@ -2,7 +2,18 @@
 
 int main() {
     //Schreiben Sie Code, welcher die Multiplikationstabelle von 1 bis n ausgibt. Lesen Sie n ein.
+    int n;
+    std::cout << "Bitte geben Sie eine ganze Zahl ein: ";
+    std::cin >> n;
 
+    for (int i1=1; i1<n+1; ++i1) {
+        for (int i2=1; i2<n+1; ++i2){
+            if(i2==n){
+                std::cout << i1 * i2 << std::endl;
+            } else
+                std::cout << i1 * i2 << " ";
+        }
+    }
     
     return 0;
 }
