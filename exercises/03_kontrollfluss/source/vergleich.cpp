3c64840160ac2a70fee4a4da787e33850e27093b
jsbinarywizard <s6jtscmi@uni-bonn.de>
2025-02-17 09:56:08 +0100

    Aufgaben Tag 03

---
+++ exercises/03_kontrollfluss/source/vergleich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    //Fuer die erste Aufgabe wird eine Zahl eingelesen. Diese wird dann auch nochmal auf dem Terminal ausgeben zum ueberpruefen.
+    int zahl;
+    std::cout << "Bitte geben Sie eine ganze Zahl ein: ";
+    // Wie liest man nochmal eine Zahl in "zahl" ein?
+    std::cout << "Die eingegebene Zahl lautet: " << zahl << std::endl;
+
+    //Erste Teilaufgabe: Ueberpruefe ob die Zahl groessen, kleiner oder gleich 0 ist. Gebt dazu das Ergebnis aus.
+
+
+
+
+    //Zweite Teilaufgabe: Bestimmt ob die Zahl gerade ist
+
+    return 0;
+}
