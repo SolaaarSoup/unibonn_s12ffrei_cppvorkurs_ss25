c3e57472e1932661c96fa89e8e5c8b246e49698d
SolarSoup <felixfreis@gmail.com>
2025-02-17 15:14:24 +0100

    Vergleich Aufgabe bearbeitet

--- exercises/03_kontrollfluss/source/vergleich.cpp
+++ exercises/03_kontrollfluss/source/vergleich.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 int main() {
     //Fuer die erste Aufgabe wird eine Zahl eingelesen. Diese wird dann auch nochmal auf dem Terminal ausgeben zum ueberpruefen.
@@ -9,10 +10,22 @@ int main() {
 
     //Erste Teilaufgabe: Ueberpruefe ob die Zahl groessen, kleiner oder gleich 0 ist. Gebt dazu das Ergebnis aus.
 
-
+    if(zahl==0){
+        std::cout << "Die Zahl ist 0" << std::endl;
+    } else if(zahl > 0) {
+        std::cout << "Die Zahl ist groesser als 0" << std::endl;
+    } else if(zahl < 0) {
+        std::cout << "Die Zahl ist kleiner als 0" << std::endl;
+    }
 
 
     //Zweite Teilaufgabe: Bestimmt ob die Zahl gerade ist
+
+    if(zahl%2==0){
+        std::cout << "Zahl ist gerade" << std::endl;
+    } else {
+        std::cout << "Zahl ist ungerade" << std::endl;
+    }
 
     return 0;
 }
