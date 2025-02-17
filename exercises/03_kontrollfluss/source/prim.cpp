6bd2da29066fb827da5dea7400f949f989aeddc7
SolarSoup <felixfreis@gmail.com>
2025-02-17 16:02:00 +0100

    primzahlen gemacht

--- exercises/03_kontrollfluss/source/prim.cpp
+++ exercises/03_kontrollfluss/source/prim.cpp
@@ -4,5 +4,19 @@ int main() {
     //Aufgabe 3: Schreiben einen Code, welcher ueberprueft ob die Zahl eine Primzahl ist.
     //Auch hier wieder die Zahl n einlesen über das Terminal.
 
+    long zahl; 
+    std::cout << "Bitte geben Sie eine ganze Zahl ein: ";
+    std::cin >> zahl;
+
+    for (long i=2; i<zahl+1; ++i) {
+        if(i==zahl) {
+            std::cout << zahl << " ist eine Primzahl!" << std::endl;
+        }
+        else if(zahl%i == 0) {
+            std::cout << zahl << " ist keine Primzahl!" << std::endl;
+            break;
+        }
+    }
+
     return 0;
 }
