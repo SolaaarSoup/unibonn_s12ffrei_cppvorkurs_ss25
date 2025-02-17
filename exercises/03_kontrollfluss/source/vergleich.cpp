ce8264549fcf3e8602d4986038878d7f190e2476
Justin Schmitz <s6jtscmi@uni-bonn.de>
2025-02-17 11:00:17 +0000

    CIN in der ersten Aufgabe (Tag 03)

--- exercises/03_kontrollfluss/source/vergleich.cpp
+++ exercises/03_kontrollfluss/source/vergleich.cpp
@@ -4,7 +4,7 @@ int main() {
     //Fuer die erste Aufgabe wird eine Zahl eingelesen. Diese wird dann auch nochmal auf dem Terminal ausgeben zum ueberpruefen.
     int zahl;
     std::cout << "Bitte geben Sie eine ganze Zahl ein: ";
-    // Wie liest man nochmal eine Zahl in "zahl" ein?
+    std::cin >> zahl;
     std::cout << "Die eingegebene Zahl lautet: " << zahl << std::endl;
 
     //Erste Teilaufgabe: Ueberpruefe ob die Zahl groessen, kleiner oder gleich 0 ist. Gebt dazu das Ergebnis aus.
