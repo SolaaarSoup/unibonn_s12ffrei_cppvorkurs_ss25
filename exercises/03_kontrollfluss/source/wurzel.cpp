877f41d74d41acede6b0780d83640829b93abe98
SolarSoup <felixfreis@gmail.com>
2025-02-18 14:08:48 +0100

    wurzel aufgabe gemacht

--- exercises/03_kontrollfluss/source/wurzel.cpp
+++ exercises/03_kontrollfluss/source/wurzel.cpp
@@ -12,7 +12,17 @@ int main() {
     int s;
     std::cout << "Bitte geben Sie eine ganze Zahl ein: ";
     std::cin >> s;
+    int n;
+    std::cout << "Wie viele Iterationen?: ";
+    std::cin >> n;
 
+    double x_i {static_cast<double>(s)};
+
+    for(long i=1; i<n; ++i) {
+        x_i = (0.5) * (x_i + s / x_i);
+    }
+
+    std::cout << "Die approximierte Wurzel von " << s << " nach " << n << " Iterationen, ist: " << x_i << std::endl;
 
     return 0;
 }
