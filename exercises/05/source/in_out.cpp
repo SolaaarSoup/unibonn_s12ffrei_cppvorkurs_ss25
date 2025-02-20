9201dc4dc5a7a12d1e73766b02bfd60581bd6819
SolarSoup <felixfreis@gmail.com>
2025-02-20 15:16:01 +0100

    finished in_out.cpp

--- exercises/05/source/in_out.cpp
+++ exercises/05/source/in_out.cpp
@@ -3,15 +3,29 @@
 #include <vector>
 
 // Schreiben Sie eine Funktion, welche die Fibonacci Zahlen bestimmt
-int fibonacci(int n) {
-
+long fibonacci(int n) {
+    if(n==1||n==2) {
+        return 1;
+    }
+    if(n<=0) {
+        return 0;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
+
+
 
 //Schreiben Sie eine Funktion, welche Rekursiv die Potenz bestimmt
 //Wir betrachten nur nicht negative exponenten
 int potenz(int n, int exponent) {
-
+    int result {1};
+    for(int i=1; i<=exponent; ++i) {
+        result *= n;
+    }
+    return result;
 }
+
 /*
 Lesen Sie die Datei "input.txt" ein.
 Bestimmen für jede Zahl in dieser Datei die Fibonacci Zahl und die vierte Potenz.
@@ -19,6 +33,15 @@ Schreiben Sie diese Zahl, die Fibonacci Zahl und die Potenz in eine csv-Datei.
 Halten Sie sich an die Konvention der csv-Dateien.
 */
 int main() {
- 
+    std::ifstream input("../input/input.txt");
+    std::ofstream output("output.csv");
+
+    output << "n, sqr, fib" << std::endl;
+    int n; 
+    while(input >> n){
+        output << n << ", " << potenz(n, 4) << ", " << fibonacci(n) << std::endl;
+    }
+
+    output.close();
     return 0;
 }
