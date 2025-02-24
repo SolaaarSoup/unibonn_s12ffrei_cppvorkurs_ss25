0abf96aa49df61a0c9e228eba75e814ade243976
SolarSoup <felixfreis@gmail.com>
2025-02-24 14:29:52 +0100

    finished vector.cpp

--- exercises/08/source/vector.cpp
+++ exercises/08/source/vector.cpp
@@ -12,7 +12,27 @@ Geben Sie den Vektor nun in Umgekehrter Reihenfolge wieder aus.
 using namespace std;
 
 int main() {
+    std::vector<double> vec;
+    std::ifstream input("../input/input.txt");
 
+    double i;
+    double sum;
+
+    while(input >> i){
+        vec.push_back(i);
+    }
+
+    for(double num: vec){
+        sum += num;
+        }
     
+    std::cout << "Länge: " << vec.size() << std::endl;
+
+    std::cout << "Durchschnitt: " << sum/vec.size() << std::endl;
+
+    for(auto it = vec.rbegin(); it != vec.rend(); ++it){
+        std::cout << *it << std::endl;
+    }
+
     return 0;
 }
