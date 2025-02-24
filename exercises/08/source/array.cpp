3af06747c3a27fd2458d3379b262734636a4023b
SolarSoup <felixfreis@gmail.com>
2025-02-24 14:00:21 +0100

    finished array.cpp

--- exercises/08/source/array.cpp
+++ exercises/08/source/array.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <numeric>
+#include <array>
 using namespace std;
 
 /*
@@ -8,10 +10,30 @@ Erstellen Sie eine Funktion, welche ein Array übergeben bekommt und die geraden
 Geben Sie das überarbeite Array auf der Konsole aus.
 */
 
+const int n = 10; // Anzahl der Elemente im Array
+
+void change_array(std::array<int, 10> &arr){
+    for(int i {0}; i<n; i++){
+    if(arr[i]%2 == 0){
+        arr[i]*=2;
+    } else {
+        arr[i]++;
+    }
+    }
+
+}
 
 int main() {
-    const int n = 10; // Anzahl der Elemente im Array
+    
+    
+    std::array<int, n> num_n = {};
+    std::iota(num_n.begin(), num_n.end(), 1);
 
+    change_array(num_n);
+
+    for(int num: num_n){
+    std::cout << num << std::endl;
+    }
 
     return 0;
 }
