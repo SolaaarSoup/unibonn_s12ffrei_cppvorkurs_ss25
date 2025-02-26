ae67203d8e2245ce39f8ae3a0c4675e21e552d32
SolarSoup <felixfreis@gmail.com>
2025-02-26 14:33:18 +0100

    finished integrate.cpp

--- exercises/05/source/integrate.cpp
+++ exercises/05/source/integrate.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <functional>
 
 /*
 In dieser Aufgabe ist wollen wir Integrieren. 
@@ -12,6 +13,8 @@ double simpleFunction(double x) {
     return x * x;
 }
 
+//std::function<double (double)> simp =
+
 // Schwierige Funktion: f(x) = exp(-x^2) * cos(x)
 double complicatedFunction(double x) {
     return std::exp(-x * x) * std::cos(x);
@@ -19,10 +22,27 @@ double complicatedFunction(double x) {
 
 //Implementieren Sie das Trapezverfahren
 
+double trapez(std::function<double (double)> f, double a, double b, uint n){
+    double h {(b - a)/n};
+    double res {0.5 * h * (f(b) - f(a))};
+    for(uint i {0}; i<n; i++){
+        res += (h * f(a + i*h));
+    }
+    
+    return res;
+}
 
 
 //Implementieren Sie die Simpson-Regel
 
+double simpson(std::function<double (double)> f, double a, double b, uint n){
+    double h {(b - a)/n};
+    double res;
+    for(uint i {0}; i<n; i++){
+        res += (h/6) *(f(a + i*h) + 4 * f((2*a + (2*i + 1)*h)/2) + f(a + (i+1)*h));
+    }
+    return res;
+}
 
 
 //Bonus Aufgabe:
@@ -33,7 +53,19 @@ int main() {
     // Integrationsgrenzen
     double a = 0.0;
     double b = 3.0;
+    uint N = 10000000;
 
-    
+    double res_sf_theo = 9.0;
+    double res_cf_theo = 0.69021;
+
+    double res_sf_simp = simpson(simpleFunction, a, b ,N);
+    double res_cf_simp = simpson(complicatedFunction, a , b, N);
+
+    double res_sf_trap = trapez(simpleFunction, a, b ,N);
+    double res_cf_trap = trapez(complicatedFunction, a , b, N);
+
+    std::cout << "Theoretisch: Simple " << res_sf_theo << ", Complicated " << res_cf_theo << std::endl;
+    std::cout << "Trapez: Simple " << res_sf_trap << ", Complicated " << res_cf_trap << std::endl;
+    std::cout << "Simpson: Simple " << res_sf_simp << ", Complicated " << res_cf_simp << std::endl;
     return 0;
 }
